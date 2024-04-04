d07a726595414af480fabb2b4bef0198c0c0ba38
cat2double <cat2double@yandex.ru>
2024-04-05 00:06:33 +0300

    1

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const schedule = require("node-schedule")
+const func = require("./func.js");
+
 console.log("++++++++ Start ++++++++")
+
+schedule.scheduleJob(
+    { minute: new schedule.Range(0, 55, 5), second: 15, tz: "Europe/Moscow" },
+    async function () {
+        console.log(func.mscDate())
+
+    })
+
