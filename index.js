45f1df5893777824158aa4e0e234897f416574d5
cat2double <cat2double@yandex.ru>
2024-04-05 04:46:36 +0300

    1

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const schedule = require("node-schedule")
 const func = require("./func.js");
+const {Global} = require("./global.js");
+const config = require('./config.json'); // Подключаем файл с параметрами и информацией
+const prefix = config.prefix; // «Вытаскиваем» префикс
 
+global.local = process.env.COMPUTERNAME == "CAT2-D"
+let token, vk_token
+if (global.local) {
+    token  = config.token 
+    vk_token  = config.vk_token
+}
+else {
+    token  = process.env.token 
+    vk_token  = process.env.vk_token
+}
 console.log("++++++++ Start ++++++++")
 
+console.log(token)
+/*
 schedule.scheduleJob(
     { minute: new schedule.Range(0, 55, 5), second: 15, tz: "Europe/Moscow" },
     async function () {
@@ -10,3 +25,4 @@ schedule.scheduleJob(
 
     })
 
+*/
