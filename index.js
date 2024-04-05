dd9f386b78c3f2d63f5439b23728d0c9ba658d03
cat2double <cat2double@yandex.ru>
2024-04-05 04:59:39 +0300

    1

--- index.js
+++ index.js
@@ -4,25 +4,23 @@ const {Global} = require("./global.js");
 
 
 global.local = process.env.COMPUTERNAME == "CAT2-D"
-let token, vk_token
+
 if (global.local) {
     const config = require('./config.json'); // Подключаем файл с параметрами и информацией
-    token  = config.token 
-    vk_token  = config.vk_token
+    global.token  = config.token 
+    global.vk_token  = config.vk_token
 }
 else {
-    token  = process.env.token 
-    vk_token  = process.env.vk_token
+    global.token  = process.env.token 
+    global.vk_token  = process.env.vk_token
 }
 console.log("++++++++ Start ++++++++")
 
-console.log(token)
-/*
+console.log(global.token)
+
 schedule.scheduleJob(
     { minute: new schedule.Range(0, 55, 5), second: 15, tz: "Europe/Moscow" },
     async function () {
         console.log(func.mscDate())
 
     })
-
-*/
